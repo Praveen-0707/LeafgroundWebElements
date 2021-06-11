08837f5211d0945fed7fee52dfdb075d8af737b1
aRRaj <aRRaj@LAPTOP-4GCDTFLO>
2021-06-12 00:12:59 +0530

    Code Changes

---
+++ src/main/java/leafground/page/webelements/HandlingExplicitWaits.java
@@ -0,0 +1,10 @@
+package leafground.page.webelements;
+
+public class HandlingExplicitWaits {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/leafground/page/webelements/HandlingFileUploadAndDownload.java
@@ -0,0 +1,10 @@
+package leafground.page.webelements;
+
+public class HandlingFileUploadAndDownload {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
